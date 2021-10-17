f53f414d54f47b9e3c9aaa9705c05581100b5922
TheUltimateOptimist <digitalConfidence@web.de>
2021-10-17 10:59:43 +0200

    added language modul

---
+++ language.py
@@ -0,0 +1,4 @@
+matrixChecks = ["symmetrisch", "quadratisch", "Einheitsmatrix", "Diagonalmatrix",
+                "Nullmatrix", "untereDreiecksmatrix", "obereDreiecksmatrix", "invertierbar", "Eigenschaften"]
+generateMatrixCommands = ["Benutzereingabe",
+                          "symmetrischeMatrix", "obereDreiecksmatrix", "untereDreiecksmatrix", "Einheitsmatrix", "Nullmatrix", "Diagonalmatrix", "quadratischeMatrix", "ZufallsMatrix"]
